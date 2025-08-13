74cea4762de689faf4becfa10b60759dadbdd10d
indradwii <indradwii53@gmail.com>
2025-08-13 21:34:32 +0700

    deploy homepage caraousel dan tab admin toko

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -285,6 +285,20 @@ const HomePage = ({
               className="carousel slide shadow-lg rounded-4"
               data-bs-ride="carousel"
             >
+              <div className="carousel-indicators">
+                {banners.map((banner, index) => (
+                  <button
+                    type="button"
+                    data-bs-target="#heroBannerCarouselMobile"
+                    data-bs-slide-to={index}
+                    className={index === 0 ? "active" : ""}
+                    aria-current={index === 0 ? "true" : "false"}
+                    aria-label={`Slide ${index + 1}`}
+                    key={banner.id}
+                  ></button>
+                ))}
+              </div>
+
               <div className="carousel-inner rounded-4">
                 {banners.map((banner, index) => (
                   <div
@@ -303,6 +317,30 @@ const HomePage = ({
                   </div>
                 ))}
               </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#heroBannerCarouselMobile"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#heroBannerCarouselMobile"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
             </div>
           )}
         </div>
