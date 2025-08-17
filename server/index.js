0761922678755f523196a0896ecff16e801dc41a
evelotion <indradwigaming@gmail.com>
2025-08-17 17:31:20 +0700

    testing origin *

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ const PORT = 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://stridebase-client-clct.onrender.com"],
+    origin: "*",
      methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
      allowedHeaders: ["Content-Type", "Authorization"], // <-- TAMBAHKAN BARIS INI
   },
@@ -111,7 +111,7 @@ io.on("connection", (socket) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://stridebase-client-clct.onrender.com"],
+    origin: "*",
     allowedHeaders: ["Content-Type", "Authorization"], // <-- TAMBAHKAN BARIS INI
   })
 );
