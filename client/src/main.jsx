5c6afdd75380194cab19704ee8e317485fd5b909
evelotion <indradwigaming@gmail.com>
2025-09-01 21:15:47 +0700

    deploy ulang

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,6 @@ import { HelmetProvider } from "react-helmet-async";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./style.css";
-import './assets/css/custom-auth.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
