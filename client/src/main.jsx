356c74919165ae479b8fe65923efdf6521aed48e
evelotion <indradwigaming@gmail.com>
2025-09-11 21:49:38 +0700

    push 3

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,16 @@
-// File: client/src/main.jsx (Versi Perbaikan Final)
+// File: client/src/main.jsx (Perbaikan Final)
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
 
-// Pastikan urutan impor CSS sudah benar:
-// 1. Library (Bootstrap)
-// 2. Custom Styles (style.css, admin.css, dll.) agar bisa menimpa Bootstrap jika perlu
+// HANYA impor stylesheet yang bersifat global di sini
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./style.css";
-import "./admin.css"; // Impor admin.css juga di sini untuk konsistensi
+import "./style.css"; // style.css adalah file gaya utama kita
+
+// Baris 'import "./admin.css";' telah dihapus dari sini
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
